feae7349b6fda3ffe8e9763c3552daa5a4f3568d
sanjayjakhar33 <sanjayjakhar33@gmail.com>
2025-07-26 11:11:33 +0530

    Update AdminUser.js

--- models/AdminUser.js
+++ models/AdminUser.js
@@ -1,12 +1,35 @@
-const mongoose = require('mongoose');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
 
-const adminUserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String },
-  password: { type: String, required: true },
-  otp: { type: String },
-  otpExpires: { type: Date }
-}, { timestamps: true });
+const AdminUser = sequelize.define('AdminUser', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  otp: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  otpExpires: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  timestamps: true
+});
 
-module.exports = mongoose.model('AdminUser', adminUserSchema);
+module.exports = AdminUser;
